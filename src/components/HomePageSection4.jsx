37a8fbe6c90964ed0c65b80e24bf8fc3f48ede9f
SAKSHAM PARDESI <sakshampardesi5831@gmail.com>
2023-09-08 11:32:54 +0530

    section3 started

--- src/components/HomePageSection4.jsx
+++ src/components/HomePageSection4.jsx
@@ -1,36 +1,75 @@
-import React, { Fragment } from 'react'
-import {Container,Col,Row} from 'react-bootstrap'
+import React, { Fragment } from "react";
+import { Container, Col, Row, Button } from "react-bootstrap";
 
-const Section4Container={
-  minHeight:"100vh",
-  border:"2px solid red"
-}
-const Section4Row={
-  minHeight:"100vh",
-  border:"2px solid red"
-}
-const Section4Col={
-  height:'100vh',
-  border:"2px solid #000",
-  
-}
+const Section4Container = {
+  minHeight: "80vh",
+  border: "2px solid red",
+};
+const Section4Row = {
+  minHeight: "80vh",
+  border: "2px solid red",
+};
+const Section4Col = {
+  height: "80vh",
+  border: "2px solid #000",
+};
 const HomePageSection4 = () => {
   return (
     <div>
       <Fragment>
-         <Container fluid style={Section4Container}>
+        <Container fluid style={Section4Container}>
           <Row style={Section4Row}>
-            <Col style={Section4Col} xxl={6} xl={6} lg={6} md={12} sm={12} xs={12}>
-                <div></div>
+            <Col
+              style={Section4Col}
+              xxl={6}
+              xl={6}
+              lg={6}
+              md={12}
+              sm={12}
+              xs={12}
+            >
+              <div className="section4col">
+                <div className="section4ColContentWrapper">
+                  <h1>
+                    Responsible Credit <br /> Redefined:
+                  </h1>
+                  <p>
+                  At Credmudra, responsible credit means more than just borrowing. We're your advocates, understanding your needs and matching you with the right financial products. Our commitment spans from financial literacy to timely issue resolution, ensuring you always have a trusted partner by your side.
+                  </p>
+                   <div className="buttonWrapper">
+                       <button>Get Match Now!</button>
+                   </div>
+                </div>
+                
+                {/* <h1>
+                  Responsible Credit <br /> Redefined:
+                </h1>
+                <p>
+                  At Credmudra, responsible credit means more than just
+                  borrowing. We're your advocates, understanding your needs and
+                  matching you with the right financial products. Our commitment
+                  spans from financial literacy to timely issue resolution,
+                  ensuring you always have a trusted partner by your side
+                </p>
+                <button>
+                  <a href="">Get Matched Now!</a> 
+                </button> */}
+              </div>
             </Col>
-            <Col style={Section4Col} xxl={6} xl={6} lg={6} md={12} sm={12} xs={12}>
-            
-            </Col>
-           </Row>
-         </Container>
+            <Col
+              style={Section4Col}
+              xxl={6}
+              xl={6}
+              lg={6}
+              md={12}
+              sm={12}
+              xs={12}
+            ></Col>
+          </Row>
+        </Container>
       </Fragment>
     </div>
-  )
-}
+  );
+};
 
-export default HomePageSection4
+export default HomePageSection4;
